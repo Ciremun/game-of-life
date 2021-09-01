7c3f51fe4ed6287eaeb37ce6d81825d65b81ac1d
Ciremun <Ciremun@yandex.ru>
2021-09-02 01:19:47 +0300

    wasm init

--- build_win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-mkdir build
-cd build/
-
-cl.exe ../gol.c /FC /nologo /Fegol.exe
-
-cd ../
--- gol.c
+++ gol.c
@@ -1,21 +1,22 @@
-//Copyright (c) 2011-2020 <>< Charles Lohr - Under the MIT/x11 or NewBSD License you choose.
+// Copyright (c) 2011-2020 <>< Charles Lohr - Under the MIT/x11 or NewBSD
+// License you choose.
 // NO WARRANTY! NO GUARANTEE OF SUPPORT! USE AT YOUR OWN RISK
 
-
+#ifndef __wasm__
+#include "os_generic.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
-#include "rawdraw/os_generic.h"
+#endif // __wasm__
 
 #ifdef _MSC_VER
 #pragma comment(lib, "gdi32")
 #pragma comment(lib, "User32")
-#endif
+#endif // _MSC_VER
 
 #define CNFG_IMPLEMENTATION
-
-#include "rawdraw/CNFG.h"
+#include "rawdraw_sf.h"
 
 #define WINDOW_NAME "Cellular Automatons"
 #define MAX_MESSAGE_SIZE 256
@@ -48,12 +49,12 @@
 #define MINUS_KEY 45
 #define PLUS_KEY 43
 #define EQ_KEY 61
-#endif
+#endif // _WIN32
 
 #define GRID_SIZE(gs) (sizeof(int) * gs * gs)
 
-int *grid = NULL;
-int *next_grid = NULL;
+int *grid = 0;
+int *next_grid = 0;
 int grid_size = DEFAULT_GRID_SIZE;
 int gamemode = GAME_OF_LIFE;
 int paused = 0;
@@ -74,7 +75,7 @@ int paused_t_width = 350;
 #else
 int font_size = 10;
 int paused_t_width = 200;
-#endif
+#endif // __ANDROID__
 
 typedef struct
 {
@@ -84,23 +85,52 @@ typedef struct
     int state;
 } Animation;
 
-Animation pause_a = {.color = 0xffffffff, .duration = .5, .start = 0.0, .state = HIDDEN};
-Animation message_a = {.color = 0xffffffff, .duration = 1.0, .start = 0.0, .state = HIDDEN};
+Animation pause_a = {
+    .color = 0xffffffff, .duration = .5, .start = 0.0, .state = HIDDEN};
+Animation message_a = {
+    .color = 0xffffffff, .duration = 1.0, .start = 0.0, .state = HIDDEN};
 
-void toggle_cell(int x, int y, int val);
+unsigned long long int gol_strlen(const char *s)
+{
+    unsigned long long int sz = 0;
+    while (s[sz] != '\0')
+        sz++;
+    return sz;
+}
+
+void *gol_memset(void *dest, int val, unsigned long long int len)
+{
+    unsigned char *ptr = dest;
+    while (len-- > 0)
+        *ptr++ = val;
+    return dest;
+}
+
+void *gol_memcpy(void *dst, void const *src, unsigned long long int size)
+{
+    unsigned char *source = (unsigned char *)src;
+    unsigned char *dest = (unsigned char *)dst;
+    while (size--)
+        *dest++ = *source++;
+    return dst;
+}
 
 void change_animation_state(Animation *a, int new_state)
 {
+#ifndef __wasm__
     a->start = OGGetAbsoluteTime();
+#endif // __wasm__
     a->state = new_state;
 }
 
 void display_message(char *msg)
 {
-    size_t msg_size = strlen(msg) + 1;
-    memset(message, 0, MAX_MESSAGE_SIZE);
-    memcpy(message, msg, msg_size);
+    unsigned long long int msg_size = gol_strlen(msg) + 1;
+    gol_memset(message, 0, MAX_MESSAGE_SIZE);
+    gol_memcpy(message, msg, msg_size);
+#ifndef __wasm__
     message_t = OGGetAbsoluteTime();
+#endif // __wasm__
     change_animation_state(&message_a, FADE_IN);
 }
 
@@ -119,7 +149,8 @@ void set_fade_color(Animation *a)
         }
         else
         {
-            new_color = (a->color & 0xffffff00) + (s_passed / a->duration) * 255;
+            new_color =
+                (a->color & 0xffffff00) + (s_passed / a->duration) * 255;
         }
     }
     break;
@@ -133,7 +164,8 @@ void set_fade_color(Animation *a)
         }
         else
         {
-            new_color = (a->color & 0xffffff00) + ((a->duration - s_passed) / a->duration) * 255;
+            new_color = (a->color & 0xffffff00) +
+                        ((a->duration - s_passed) / a->duration) * 255;
         }
     }
     break;
@@ -141,12 +173,13 @@ void set_fade_color(Animation *a)
         new_color = a->color;
         break;
     default:
-        printf("invalid animation state\n");
-        exit(1);
+    {
+    }
     }
     CNFGColor(new_color);
 }
 
+#ifndef __wasm__
 void change_grid_size(int new_size)
 {
     grid_size = new_size;
@@ -154,14 +187,19 @@ void change_grid_size(int new_size)
     cell_height = h / grid_size;
     grid = realloc(grid, GRID_SIZE(grid_size));
     next_grid = realloc(next_grid, GRID_SIZE(grid_size));
-    memset(grid, 0, GRID_SIZE(grid_size));
-    memset(next_grid, 0, GRID_SIZE(grid_size));
+    gol_memset(grid, 0, GRID_SIZE(grid_size));
+    gol_memset(next_grid, 0, GRID_SIZE(grid_size));
     snprintf(message, MAX_MESSAGE_SIZE, "%s: %d", "Grid Size", grid_size);
     message_t = OGGetAbsoluteTime();
     change_animation_state(&message_a, FADE_IN);
 }
+#endif // __wasm__
 
-void HandleKey(int keycode, int bDown)
+void
+#ifdef __wasm__
+    __attribute__((export_name("HandleKey")))
+#endif // __wasm__
+    HandleKey(int keycode, int bDown)
 {
     if (bDown)
         switch (keycode)
@@ -185,9 +223,12 @@ void HandleKey(int keycode, int bDown)
             }
             break;
         case R_KEY:
-            memset(grid, 0, GRID_SIZE(grid_size));
+            gol_memset(grid, 0, GRID_SIZE(grid_size));
+#ifndef __wasm__
             reset_t = OGGetAbsoluteTime();
+#endif // __wasm__
             break;
+#ifndef __wasm__
         case MINUS_KEY:
         {
             int new_size = grid_size - GRID_SIZE_CHANGE_STEP;
@@ -204,6 +245,7 @@ void HandleKey(int keycode, int bDown)
         case PLUS_KEY:
             change_grid_size(grid_size + GRID_SIZE_CHANGE_STEP);
             break;
+#endif // __wasm__
         case ONE_KEY:
             gamemode = GAME_OF_LIFE;
             display_message("Game Of Life");
@@ -213,9 +255,9 @@ void HandleKey(int keycode, int bDown)
             display_message("Brian's Brain");
             break;
         }
+#ifdef __ANDROID__
     else
     {
-#ifdef __ANDROID__
         switch (keycode)
         {
         case 10:
@@ -226,58 +268,8 @@ void HandleKey(int keycode, int bDown)
             AndroidSendToBack(1);
             break;
         }
-#endif
     }
-}
-
-void HandleButton(int x, int y, int button, int bDown)
-{
-    if (bDown)
-    {
-#ifdef __ANDROID__
-        if ((w - 100 <= x && x <= w) && (0 <= y && y <= 100))
-        {
-            keyboard_up = !keyboard_up;
-            AndroidDisplayKeyboard(keyboard_up);
-            return;
-        }
-#endif
-        toggle_cell(x, y, ALIVE);
-    }
-}
-
-void HandleMotion(int x, int y, int mask)
-{
-#ifndef __ANDROID__
-    if (!mask)
-        return;
-#endif
-    toggle_cell(x, y, ALIVE);
-}
-
-void HandleDestroy() {}
-void HandleSuspend() { suspended = 1; }
-void HandleResume() { suspended = 0; }
-
-void sleep_ms(int ms)
-{
-#ifdef _WIN32
-    _sleep(ms);
-#else
-    usleep(ms * 1000);
-#endif
-}
-
-void setup_window()
-{
-#ifdef __ANDROID__
-    CNFGSetupFullscreen(WINDOW_NAME, 0);
-    CNFGGetDimensions(&w, &h);
-#else
-    w = 1024;
-    h = 768;
-    CNFGSetup(WINDOW_NAME, w, h);
-#endif
+#endif // __ANDROID__
 }
 
 void cell_index(int x, int y, int *cell_x, int *cell_y)
@@ -288,7 +280,8 @@ void cell_index(int x, int y, int *cell_x, int *cell_y)
 
 int on_grid(int cell_x, int cell_y)
 {
-    return (0 <= cell_x && cell_x <= grid_size - 1) && (0 <= cell_y && cell_y <= grid_size - 1);
+    return (0 <= cell_x && cell_x <= grid_size - 1) &&
+           (0 <= cell_y && cell_y <= grid_size - 1);
 }
 
 void toggle_cell(int x, int y, int val)
@@ -297,6 +290,58 @@ void toggle_cell(int x, int y, int val)
     cell_index(x, y, &cell_x, &cell_y);
     if (on_grid(cell_x, cell_y))
         grid[cell_x * grid_size + cell_y] = val;
+}
+
+void
+#ifdef __wasm__
+    __attribute__((export_name("HandleButton")))
+#endif // __wasm__
+    HandleButton(int x, int y, int button, int bDown)
+{
+    if (bDown)
+    {
+#ifdef __ANDROID__
+        if ((w - 100 <= x && x <= w) && (0 <= y && y <= 100))
+        {
+            keyboard_up = !keyboard_up;
+            AndroidDisplayKeyboard(keyboard_up);
+            return;
+        }
+#endif // __ANDROID__
+        toggle_cell(x, y, ALIVE);
+    }
+}
+
+void
+#ifdef __wasm__
+    __attribute__((export_name("HandleMotion")))
+#endif // __wasm__
+    HandleMotion(int x, int y, int mask)
+{
+#ifndef __ANDROID__
+    if (!mask)
+        return;
+#endif
+    toggle_cell(x, y, ALIVE);
+}
+
+void HandleDestroy() {}
+
+#ifndef __wasm__
+void HandleSuspend() { suspended = 1; }
+void HandleResume() { suspended = 0; }
+#endif // __wasm__
+
+void setup_window()
+{
+#ifdef __ANDROID__
+    CNFGSetupFullscreen(WINDOW_NAME, 0);
+    CNFGGetDimensions(&w, &h);
+#else
+    w = 1024;
+    h = 768;
+    CNFGSetup(WINDOW_NAME, w, h);
+#endif // __ANDROID__
 }
 
 void draw_message(int x, int y, const char *t)
@@ -311,7 +356,8 @@ void draw_cell(int x, int y)
     int cell_x = x * cell_width;
     int cell_y = y * cell_height;
 
-    CNFGTackRectangle(cell_x, cell_y, cell_x + cell_width, cell_y + cell_height);
+    CNFGTackRectangle(cell_x, cell_y, cell_x + cell_width,
+                      cell_y + cell_height);
 }
 
 int count_neighbours(int x, int y)
@@ -320,7 +366,8 @@ int count_neighbours(int x, int y)
 
     for (int i = x - 1; i <= x + 1; i++)
         for (int j = y - 1; j <= y + 1; j++)
-            if (on_grid(i, j) && (i != x || j != y) && grid[i * grid_size + j] == ALIVE)
+            if (on_grid(i, j) && (i != x || j != y) &&
+                grid[i * grid_size + j] == ALIVE)
                 neighbours++;
 
     return neighbours;
@@ -330,12 +377,14 @@ void game_of_life_rules(int x, int y, int nbors)
 {
     switch (grid[x * grid_size + y])
     {
-        case ALIVE:
-            if (!(nbors == 2 || nbors == 3)) next_grid[x * grid_size + y] = DEAD;
-            break;
-        case DEAD:
-            if (nbors == 3) next_grid[x * grid_size + y] = ALIVE;
-            break;
+    case ALIVE:
+        if (!(nbors == 2 || nbors == 3))
+            next_grid[x * grid_size + y] = DEAD;
+        break;
+    case DEAD:
+        if (nbors == 3)
+            next_grid[x * grid_size + y] = ALIVE;
+        break;
     }
 }
 
@@ -343,15 +392,16 @@ void brians_brain_rules(int x, int y, int nbors)
 {
     switch (grid[x * grid_size + y])
     {
-        case ALIVE:
-            next_grid[x * grid_size + y] = DYING;
-            break;
-        case DYING:
-            next_grid[x * grid_size + y] = DEAD;
-            break;
-        case DEAD:
-            if (nbors == 2) next_grid[x * grid_size + y] = ALIVE;
-            break;
+    case ALIVE:
+        next_grid[x * grid_size + y] = DYING;
+        break;
+    case DYING:
+        next_grid[x * grid_size + y] = DEAD;
+        break;
+    case DEAD:
+        if (nbors == 2)
+            next_grid[x * grid_size + y] = ALIVE;
+        break;
     }
 }
 
@@ -371,7 +421,7 @@ void apply_game_rules(int x, int y)
 
 void draw_cells()
 {
-    memcpy(next_grid, grid, GRID_SIZE(grid_size));
+    gol_memcpy(next_grid, grid, GRID_SIZE(grid_size));
     for (int y = 0; y < grid_size; ++y)
         for (int x = 0; x < grid_size; ++x)
         {
@@ -380,7 +430,7 @@ void draw_cells()
             if (grid[x * grid_size + y] == ALIVE)
                 draw_cell(x, y);
         }
-    memcpy(grid, next_grid, GRID_SIZE(grid_size));
+    gol_memcpy(grid, next_grid, GRID_SIZE(grid_size));
 }
 
 void draw_messages()
@@ -398,7 +448,7 @@ void draw_messages()
             change_animation_state(&message_a, FADE_OUT);
         }
         set_fade_color(&message_a);
-        draw_message(w / 2 - strlen(message) * 30, 120, message);
+        draw_message(w / 2 - gol_strlen(message) * 30, 120, message);
     }
     if (reset_t)
     {
@@ -414,7 +464,11 @@ void draw_messages()
     }
 }
 
-int main()
+int
+#ifdef __wasm__
+    __attribute__((export_name("main")))
+#endif // __wasm__
+    main()
 {
     CNFGBGColor = 0x000080ff;
     setup_window();
@@ -422,30 +476,47 @@ int main()
     cell_width = w / grid_size;
     cell_height = h / grid_size;
 
+#ifndef __wasm__
     grid = calloc(1, GRID_SIZE(grid_size));
     next_grid = calloc(1, GRID_SIZE(grid_size));
+#else
+    int g[DEFAULT_GRID_SIZE * DEFAULT_GRID_SIZE];
+    int ng[DEFAULT_GRID_SIZE * DEFAULT_GRID_SIZE];
+    gol_memset(g, 0, GRID_SIZE(DEFAULT_GRID_SIZE));
+    gol_memset(ng, 0, GRID_SIZE(DEFAULT_GRID_SIZE));
+#endif // __wasm__
 
     display_message("Game Of Life");
 
+#ifdef RAWDRAW_USE_LOOP_FUNCTION
+    return 0;
+}
+int __attribute__((export_name("loop"))) loop()
+{
+#else
     while (1)
+#endif
     {
         CNFGClearFrame();
         CNFGHandleInput();
 
+#ifndef __wasm__
         if (suspended)
             continue;
-
         if (!paused)
-            sleep_ms(50);
+            OGUSleep(50000);
+#endif // __wasm__
 
         CNFGColor(0xff00ffff);
         draw_cells();
 
+#ifndef __wasm__
         absolute_time = OGGetAbsoluteTime();
+#endif // __wasm__
         draw_messages();
 
         CNFGSwapBuffers();
     }
 
-    return 0;
+    return (0);
 }
